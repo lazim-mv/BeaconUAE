fb048ac2573d11017e4620b86fb4f7b0bd29ef81
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-10-18 21:01:15 +0530

    v1.0.4

--- src/app/components/Jurisdiction.js
+++ src/app/components/Jurisdiction.js
@@ -8,19 +8,19 @@ function Jurisdiction() {
 
   const pageData = [
     {
-      img: "/jurisdiction/2.svg",
+      img: "/Jurisdiction/2.svg",
       heading: "Mainland Company",
       description:
         "Enjoy your freedom to establish and conduct business anywhere in the UAE with a broad scope of business and trade activities both within and outside the UAE.",
     },
     {
-      img: "/jurisdiction/1.svg",
+      img: "/Jurisdiction/1.svg",
       heading: "Free Zone Company",
       description:
         "Unveil the ideal jurisdiction for your business across 50 free zones of UAE with stunning low tax rates, and full foreign ownership, to explore the business ecosystem.",
     },
     {
-      img: "/jurisdiction/3.svg",
+      img: "/Jurisdiction/3.svg",
       heading: "Offshore company",
       description:
         "Trigger your business even without a physical presence in the UAE. Enjoy convenient access to all business services including consultancy, legal support, and more.",
