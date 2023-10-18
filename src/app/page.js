a55b46eb2ceac3d1c8457a22ff246fbf92104939
lazim-mv <106281909+lazim-mv@users.noreply.github.com>
2023-10-18 20:34:31 +0530

    v1.0.2

--- src/app/page.js
+++ src/app/page.js
@@ -81,13 +81,4 @@ export default function Home() {
   );
 }
 
-export async function getStaticProps() {
-  // Fetch data here (e.g., from an API)
-  const data = "Some data fetched at build time";
 
-  return {
-    props: {
-      data,
-    },
-  };
-}
