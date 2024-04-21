baf9069611f79faeeff2a47543451d2ab342e9d8
dhanush <dhanushpk50@gmail.com>
2024-04-21 18:32:36 +0530

    changes

--- src/app/Contents/PlacesContent.js
+++ src/app/Contents/PlacesContent.js
@@ -1,20 +1,20 @@
 export const placesData = [
   {
-    img: "/Jurisdiction/1.webp",
+    img: "/Jurisdiction/1.png",
     heading: "Abu Dhabi",
     Number: "01",
     description:
       "Explore the unparalleled ease of running your business in Abu Dhabi, the largest emirate in UAE, and make your business thrive amongst the best dynamic economy.",
   },
   {
-    img: "/Jurisdiction/2.webp",
+    img: "/Jurisdiction/2.png",
     heading: "Dubai",
     Number: "02",
     description:
       "Establish your business in Dubai, the tax-favorable, business-friendly and highly desirable economically diverse destination that has ranked first in ease of doing business.",
   },
   {
-    img: "/Jurisdiction/3.webp",
+    img: "/Jurisdiction/3.png",
     heading: "Sharjah",
     Number: "03",
     description:
