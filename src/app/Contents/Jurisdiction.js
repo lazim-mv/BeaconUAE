f36dea4def6da94d1be8dc5bcef7dec3997b78c8
dhanush <dhanushpk50@gmail.com>
2024-05-26 12:43:48 +0530

    changes

--- src/app/Contents/Jurisdiction.js
+++ src/app/Contents/Jurisdiction.js
@@ -1,18 +1,18 @@
 export const jurisdictionData = [
     {
-      img: "/Jurisdiction/1.webp",
+      img: "/Jurisdiction/2.jpg",
       heading: "Mainland Company",
       description:
-        "Enjoy your freedom to establish and conduct business anywhere in the UAE with a broad scope of business and trade activities both within and outside the UAE.",
+        "Enjoy your freedom to establish and conduct business anywhere in the UAE with a broad scope of business and trade activities both within and outside the UAE.   ",
     },
     {
-      img: "/Jurisdiction/3.webp",
+      img: "/Jurisdiction/6.webp",
       heading: "Free Zone Company",
       description:
         "Unveil the ideal jurisdiction for your business across 50 free zones of UAE with stunning low tax rates, and full foreign ownership, to explore the business ecosystem.",
     },
     {
-      img: "/Jurisdiction/2.webp",
+      img: "/Jurisdiction/5.webp",
       heading: "Offshore company",
       description:
         "Trigger your business even without a physical presence in the UAE. Enjoy convenient access to all business services including consultancy, legal support, and more.",
